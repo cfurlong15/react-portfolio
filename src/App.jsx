3a4f39a64a5557fa21c0c096d318fdd27c9c9529
Conor Furlong <cfurlong15@gmail.com>
2023-12-11 23:49:06 -0500

    portfolio skeleton complete

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
